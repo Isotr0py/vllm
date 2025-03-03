e523edded1c6be09d8cd553aa4ff9e582700a773
Isotr0py <2037008807@qq.com>
2025-03-03 15:31:33 +0800

    init flex attn
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -195,9 +195,15 @@ class CudaPlatformBase(Platform):
                 logger.info("Using Triton MLA backend.")
                 return "vllm.attention.backends.triton_mla.TritonMLABackend"
         if use_v1:
-            logger.info_once("Using Flash Attention backend on V1 engine.")
-            return ("vllm.v1.attention.backends.flash_attn."
-                    "FlashAttentionBackend")
+            if not cls.has_device_capability(80):
+                # Volta and Turing NVIDIA GPUs.
+                logger.info_once("Using Flex Attention backend on V1 engine.")
+                return ("vllm.v1.attention.backends.flex_attn."
+                        "FlexAttentionBackend")
+            else:
+                logger.info_once("Using Flash Attention backend on V1 engine.")
+                return ("vllm.v1.attention.backends.flash_attn."
+                        "FlashAttentionBackend")
         if selected_backend == _Backend.FLASHINFER:
             logger.info("Using FlashInfer backend.")
             return "vllm.attention.backends.flashinfer.FlashInferBackend"
